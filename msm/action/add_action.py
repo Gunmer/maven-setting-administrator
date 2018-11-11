5887f21700061c1cfd4d16ab94da9c7347a052ce
Cristiam Sosa <csosaur@gmail.com>
2018-11-11 15:02:03 +0100

    Update actions

--- msm/action/add_action.py
+++ msm/action/add_action.py
@@ -1,8 +1,9 @@
-from msm import repository
+from msm import repository, file_manager
 from msm.util import log
 
 
 def execute(args):
     log.set_config(args)
-    repository.create(args.alias, args.file)
+    file = file_manager.add_setting(args.file)
+    repository.create(args.alias, file)
     log.restore_config()
--- msm/action/use_action.py
+++ msm/action/use_action.py
@@ -1,12 +1,22 @@
-from msm import repository
+from msm import repository, file_manager
 from msm.util import log
 
 
 def execute(args):
     log.set_config(args)
-    alias, file = repository.find_selected()
-    if alias is not None:
-        repository.update(alias, 0)
 
-    repository.update(args.setting, 1)
+    selected_setting = repository.find_selected()
+    setting_to_use = repository.find_one(args.setting)
+
+    if setting_to_use is None:
+        print('Setting not found!!!')
+        return
+
+    if selected_setting is not None:
+        repository.update(selected_setting[0], 0)
+        file_manager.deactivate_setting(selected_setting[1])
+
+    repository.update(setting_to_use[0], 1)
+    file_manager.activate_setting(setting_to_use[1])
+
     log.restore_config()
