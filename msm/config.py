0c0e1dd097c15d2b64721fe7a9c1f953713c488f
Cristiam Sosa <csosaur@gmail.com>
2018-11-11 18:26:41 +0100

    Set variables

--- msm/config.py
+++ msm/config.py
@@ -1,7 +1,9 @@
-m2_path = '/tmp/.m2/'
+from pathlib import Path
+
+user_home_path = str(Path.home()) + '/'
+m2_path = user_home_path + '.m2/'
 
 msm_path = m2_path + 'msm/'
-
 database_path = msm_path + 'msm.db'
 
 log_debug_enable = False
