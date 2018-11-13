22b70c2bc2b2981c9b05eb4d344fcef560e77ee0
Cristiam Sosa <csosaur@gmail.com>
2018-11-13 22:12:31 +0100

    Show version and show help when no args

--- msa/config.py
+++ msa/config.py
@@ -3,6 +3,8 @@ from os.path import expanduser
 user_home_path = expanduser("~") + '/'
 m2_path = user_home_path + '.m2/'
 
+msa_version = '1.1.0'
+
 msa_path = m2_path + 'msa/'
 database_path = msa_path + 'msa.db'
 
