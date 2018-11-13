570aeaa31918f229f4f495f3a326f2ceaf34eff4
Cristiam Sosa <csosaur@gmail.com>
2018-11-14 00:08:18 +0100

    Set version

--- msa/config.py
+++ msa/config.py
@@ -3,7 +3,7 @@ from os.path import expanduser
 user_home_path = expanduser("~") + '/'
 m2_path = user_home_path + '.m2/'
 
-msa_version = '1.1.0'
+msa_version = '1.0.2'
 
 msa_path = m2_path + 'msa/'
 database_path = msa_path + 'msa.db'
